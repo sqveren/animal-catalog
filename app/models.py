4692f4ff6af356eb97f8b3088cbdbd50eadf1977
sqveren <sqveren@gmail.com>
2026-09-11 12:43:57 +0300

    Add new models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, MetaData, Date
 from app.database import Base
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Date, func
+from sqlalchemy.orm import relationship
 
 
 class Animal(Base):
@@ -13,4 +14,32 @@ class Animal(Base):
     status = Column(String, default="available",nullable=False)
     arrival_date = Column(Date, nullable=False)
     description = Column(String, nullable=True)
+
+    adoptions = relationship("Adoption", back_populates="animal")
     
+
+class Adopter(Base):
+    __tablename__ = "adopters"
+
+    id = Column(Integer, primary_key=True, index = True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String,nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    phone_number = Column(String, nullable=True)
+    registered_at = Column(DateTime, server_default=func.now())
+
+    adoptions = relationship("Adoption", back_populates="adopter")
+
+class Adoption(Base):
+    __tablename__ = "adoptions"
+
+    id = Column(Integer, primary_key=True, index = True)
+    animal_id = Column(Integer, ForeignKey("animals.id"), nullable=False)
+    adopter_id = Column(Integer,ForeignKey("adopters.id"),nullable=False)
+    adoption_time = Column(DateTime, server_default=func.now())
+    status = Column(String, default="pending", nullable=False)
+
+    animal = relationship("Animal", back_populates="adoptions")
+    adopter = relationship("Adopter", back_populates="adoptions")
+
+
